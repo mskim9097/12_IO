4177d32f59f0dde502057ab98aa4c24e10511596
MINSU KIM <mskim9097@gmail.com>
2023-01-03 15:17:40 +0900

    20230103 commit

--- src/edu/kh/io/run/IORun.java
+++ src/edu/kh/io/run/IORun.java
@@ -9,9 +9,9 @@ public class IORun {
 		IOService ioservice = new IOService();
 		
 		//ioservice.output1();
-		//ioservice.input1();
-		ioservice.output2();
-		ioservice.input2();
+		ioservice.input1();
+		//ioservice.output2();
+		//ioservice.input2();
 		
 	}
 
